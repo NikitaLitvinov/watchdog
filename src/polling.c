6db8a33c903f3d8b71be8092315f9f76bfea386d
Nikita Litvinov <litvinov.nikita.sergeevich@gmail.com>
2022-07-12 21:33:03 +0700

    add signal handling for exit

--- src/polling.c
+++ src/polling.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <sys/epoll.h>
-#include <sys/timerfd.h>
 #include <errno.h>
 #include <string.h>
+#include <signal.h>
+#include <stdbool.h>
+#include <sys/epoll.h>
+#include <sys/timerfd.h>
+#include <sys/signalfd.h>
 
 #include "polling.h"
 
 static inline void delete_timer(int *const timer_fd)
 {
     int ret = close(*timer_fd);
+
     if (-1 == ret)
     {
         printf("close(%d) failed. %s\n", *timer_fd, strerror(errno));
@@ -23,7 +27,8 @@ static int create_timer(int *const timer_fd, int const timer_interval)
     struct itimerspec timer = {0};
     struct timespec now = {0};
 
-    if(0 != clock_gettime(CLOCK_MONOTONIC,&now)){
+    if (0 != clock_gettime(CLOCK_MONOTONIC, &now))
+    {
         printf("clock_gettime() failed. %s\n", strerror(errno));
         return EXIT_FAILURE;
     }
@@ -34,11 +39,12 @@ static int create_timer(int *const timer_fd, int const timer_interval)
     timer.it_interval.tv_nsec = 0;
 
     *timer_fd = timerfd_create(CLOCK_MONOTONIC, TFD_NONBLOCK);
-    if (0 > *timer_fd )
+    if (0 > *timer_fd)
     {
         printf("timerfd_create failed. %s\n", strerror(errno));
         return EXIT_FAILURE;
     }
+
     if (0 > timerfd_settime(*timer_fd, TFD_TIMER_ABSTIME, &timer, NULL))
     {
         printf("timerfd_settime failed. %s\n", strerror(errno));
@@ -53,6 +59,7 @@ static int create_timer(int *const timer_fd, int const timer_interval)
 static inline void delete_polling(int *const epoll_fd)
 {
     int ret = close(*epoll_fd);
+
     if (-1 == ret)
     {
         printf("close(%d) failed. %s\n", *epoll_fd, strerror(errno));
@@ -60,19 +67,33 @@ static inline void delete_polling(int *const epoll_fd)
     *epoll_fd = -1;
 }
 
-static int create_poll(int *const epoll_fd, struct epoll_event *const events,  int const *const timer_fd)
+static int create_poll(int *const epoll_fd, int const *const timer_fd,
+                       int const *const sig_fd)
 {
-    *epoll_fd = epoll_create(1);
+    struct epoll_event event = {0};
+
+    *epoll_fd = epoll_create1(0);
     if (-1 == *epoll_fd)
     {
-        printf("epoll_create() failed. %s\n",  strerror(errno));
+        printf("epoll_create() failed. %s\n", strerror(errno));
         return EXIT_FAILURE;
     }
 
-    events->events = EPOLLIN;
-    if (0 < epoll_ctl(*epoll_fd, EPOLL_CTL_ADD, *timer_fd, events))
+    event.events = EPOLLIN;
+    event.data.fd = *timer_fd;
+    if (0 < epoll_ctl(*epoll_fd, EPOLL_CTL_ADD, *timer_fd, &event))
     {
-        printf("epoll_ctl() failed. %s\n", strerror(errno));
+        printf("epoll_ctl() for timer fd failed. %s\n", strerror(errno));
+        delete_polling(epoll_fd);
+        return EXIT_FAILURE;
+    }
+
+    event.events = EPOLLIN | EPOLLERR | EPOLLHUP;
+    event.data.fd = *sig_fd;
+
+    if (0 < epoll_ctl(*epoll_fd, EPOLL_CTL_ADD, *sig_fd, &event))
+    {
+        printf("epoll_ctl() for sig fd failed. %s\n", strerror(errno));
         delete_polling(epoll_fd);
         return EXIT_FAILURE;
     }
@@ -82,13 +103,64 @@ static int create_poll(int *const epoll_fd, struct epoll_event *const events,  i
     return EXIT_SUCCESS;
 }
 
+static int create_signal(int *const sig_fd)
+{
+    int ret = EXIT_SUCCESS;
+    sigset_t sigset = {0};
+
+    ret = sigemptyset(&sigset);
+    if (ret < 0)
+    {
+        printf("sigemptyset() failed. %s\n", strerror(errno));
+        return EXIT_FAILURE;
+    }
+
+    ret = sigaddset(&sigset, SIGTERM);
+    if (ret < 0)
+    {
+        printf("sigaddset(%s) failed. %s\n", strsignal(SIGTERM), strerror(errno));
+        return EXIT_FAILURE;
+    }
+
+    ret = sigaddset(&sigset, SIGQUIT);
+    if (ret < 0)
+    {
+        printf("sigaddset(%s) failed. %s\n", strsignal(SIGQUIT), strerror(errno));
+        return EXIT_FAILURE;
+    }
+
+    ret = sigprocmask(SIG_BLOCK, &sigset, NULL);
+    if (ret < 0)
+    {
+        printf("sigprocmask() failed. %s\n", strerror(errno));
+        return EXIT_FAILURE;
+    }
+
+    *sig_fd = signalfd(-1, &sigset, 0);
+    if (0 > *sig_fd)
+    {
+        printf("signalfd() failed. %s\n", strerror(errno));
+        return EXIT_FAILURE;
+    }
+
+    printf("Signal fd create success!\n");
+
+    return EXIT_SUCCESS;
+}
+
 int polling_pid(int const timer_interval)
 {
+    enum
+    {
+        MAX_EVENT_COUNT = 10,
+    };
+
     int ret = EXIT_SUCCESS;
     int timer_fd = 0;
     int epoll_fd = 0;
-    size_t res = 0;
-    struct epoll_event events = {0};
+    int sig_fd = 0;
+    struct epoll_event events[MAX_EVENT_COUNT] = {0};
+    bool loop = true;
 
     ret = create_timer(&timer_fd, timer_interval);
     if (EXIT_SUCCESS != ret)
@@ -96,7 +168,15 @@ int polling_pid(int const timer_interval)
         printf("create_timer() failed.\n");
         return ret;
     }
-    ret = create_poll(&epoll_fd, &events, &timer_fd);
+
+    ret = create_signal(&sig_fd);
+    if (EXIT_SUCCESS != ret)
+    {
+        printf("create_signal() failed.\n");
+        return ret;
+    }
+
+    ret = create_poll(&epoll_fd, &timer_fd, &sig_fd);
     if (EXIT_SUCCESS != ret)
     {
         printf("create_poll() failed.\n");
@@ -104,25 +184,52 @@ int polling_pid(int const timer_interval)
         return ret;
     }
 
-    while(1)
+    printf("Start polling\n");
+
+    while (loop)
     {
-        if (0 > epoll_wait(epoll_fd, &events, 1, 10000)) {
+        int read_events = epoll_wait(epoll_fd, events, MAX_EVENT_COUNT, -1);
+        if (0 > read_events)
+        {
             printf("epoll_wait() failed. %s\n", strerror(errno));
             delete_polling(&epoll_fd);
             delete_timer(&timer_fd);
             return EXIT_FAILURE;
         }
-        if (0 > read(timer_fd, &res, sizeof(res)))
+        for (int i = 0; i < read_events; i++)
         {
-            printf("read() failed. %s\n", strerror(errno));
-        }
-        else
-        {
-            printf("Time out\n");
+            if (events[i].data.fd == timer_fd)
+            {
+                size_t res = 0;
+                if (0 > read(timer_fd, &res, sizeof(res)))
+                {
+                    printf("read() failed. %s\n", strerror(errno));
+                }
+                else
+                {
+                    printf("Time out\n");
+                }
+            }
+            else if (events[i].data.fd == sig_fd)
+            {
+                struct signalfd_siginfo info = {0};
+                if (0 > read(sig_fd, &info, sizeof(info)))
+                {
+                    printf("read() failed. %s\n", strerror(errno));
+                }
+                else
+                {
+                    printf("Catch signal %d\n", info.ssi_signo);
+                    loop = false;
+                    break;
+                }
+            }
         }
     }
 
+    printf("Finish polling\n");
     delete_polling(&epoll_fd);
     delete_timer(&timer_fd);
-    return ret;
+
+    return EXIT_SUCCESS;
 }
